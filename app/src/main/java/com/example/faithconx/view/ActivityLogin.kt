9664809a6cc3b4518963a003bc1da23bf60f24e8
Bharat-Ghusi <bharatghusi@appscrip.co>
2023-07-01 16:43:07 +0530

    Designed Login page

---
+++ app/src/main/java/com/example/faithconx/view/ActivityLogin.kt
@@ -0,0 +1,12 @@
+package com.example.faithconx.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.faithconx.R
+
+class ActivityLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/faithconx/view/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.faithconx.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.faithconx.ui.theme.FaithconxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FaithconxTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FaithconxTheme {
+        Greeting("Android")
+    }
+}
