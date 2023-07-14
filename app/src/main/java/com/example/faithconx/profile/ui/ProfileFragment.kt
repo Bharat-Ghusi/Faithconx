1ff61348fdc10c5dc890c0990faeff0645f4596c
Bharat-Ghusi <bharatghusi@appscrip.co>
2023-07-14 17:38:44 +0530

    Rectify margin, space, in profile fragment

--- app/src/main/java/com/example/faithconx/profile/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/faithconx/profile/ui/ProfileFragment.kt
@@ -1,26 +1,31 @@
 package com.example.faithconx.profile.ui
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.MenuItem.OnMenuItemClickListener
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.example.faithconx.R
 import com.example.faithconx.databinding.FragmentProfileBinding
 import com.example.faithconx.helper.user.UserSession
 import com.example.faithconx.login.ui.ActivityLogin
 import com.google.firebase.auth.FirebaseAuth
 
-class ProfileFragment : Fragment() ,View.OnClickListener{
+class ProfileFragment : Fragment(), View.OnClickListener, OnMenuItemClickListener{
     private val userSession = UserSession()
-private lateinit var binding: FragmentProfileBinding
-private val firebaseAuth =FirebaseAuth.getInstance()
+    private lateinit var binding: FragmentProfileBinding
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentProfileBinding.inflate(inflater)
 
         setOnClickListener()
@@ -29,11 +34,19 @@ private val firebaseAuth =FirebaseAuth.getInstance()
     }
 
     private fun setOnClickListener() {
-    binding.btnLogout.setOnClickListener(this@ProfileFragment)
+//        binding.btnLogout.setOnClickListener(this@ProfileFragment)
+        binding.toolbarMain.setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener { onMenuItemClick(it) })
+    }
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menuLogout -> logoutUser()
+        }
+        return true
     }
 
     override fun onClick(view: View?) {
-        when(view?.id){
+        when (view?.id) {
             R.id.btnLogout -> logoutUser()
         }
     }
@@ -44,8 +57,10 @@ private val firebaseAuth =FirebaseAuth.getInstance()
         firebaseAuth.signOut()
         activity?.supportFragmentManager?.popBackStack()
         activity?.finish()
-        startActivity(Intent(activity,ActivityLogin::class.java))
+        startActivity(Intent(activity, ActivityLogin::class.java))
     }
+
+
 
 
 }
